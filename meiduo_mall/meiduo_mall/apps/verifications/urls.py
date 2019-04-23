a14c38830ddbd83f6602bb0f21128b031ba6c2c0
stormspirit2 <lqz920727@126.com>
2019-04-23 19:41:23 +0800

    验证码图形

--- meiduo_mall/meiduo_mall/apps/verifications/urls.py
+++ meiduo_mall/meiduo_mall/apps/verifications/urls.py
@@ -3,7 +3,9 @@ from django.conf.urls import url
 from . import  views
 
 urlpatterns = [
-
+    #发送图形验证码
     url(r'^image_codes/(?P<uuid>[\w-]+)/$',views.ImageCodeView.as_view()),
+    #发送短信验证吗
+    url(r'^sms_codes/(?P<mobile>1[3-9]\d{9})/$', views.SMSCodeView.as_view()),
 
 ]
